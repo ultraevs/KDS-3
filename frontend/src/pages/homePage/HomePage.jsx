7f2dd6d4120f12e16fb69b1670ebe57ed7569fd3
Konstantin <kostya1703tsoy@gmail.com>
2024-02-03 21:14:16 +0400

    fix path to CarouselBox

--- frontend/src/pages/homePage/HomePage.jsx
+++ frontend/src/pages/homePage/HomePage.jsx
@@ -5,7 +5,7 @@ import Header from '../../components/header/Header'
 import About from '../../components/about/About'
 import MoreInfo from '../../components/moreInfo/MoreInfo'
 import Projects from '../../components/projects/Projects'
-import CarouselBox from '../../components/CarouselBox/CarouselBox'
+import CarouselBox from '../../components/carouselBox/CarouselBox'
 import Partners from '../../components/partners/Partners'
 import Footer from '../../components/footer/Footer'
 
