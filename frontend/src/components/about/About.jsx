80758018eb3b1bd1d9fbc5049c2c0d2506febd51
Konstantin <kostya1703tsoy@gmail.com>
2024-02-04 00:36:45 +0400

    wrap in common container

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import styles from "./About.module.css"
 
+import classNames from 'classnames'
+
 const About = () => {
   return (
     <>
     <div className={styles.about} id='about'>
+      <div className={ classNames("container", styles.aboutZone)}>
         <h1>
-            КДС-3 - компания, занимающаяся строительством и модернизацией 
-            инфраструктуры и объектов на транспорте
+          КДС-3 - компания, занимающаяся строительством и модернизацией 
+          инфраструктуры и объектов на транспорте
         </h1>
+      </div>
     </div>
     </>
   )
--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -4,20 +4,21 @@ import styles from "./Header.module.css"
 import logo from "../../img/KDS-LOGO.png"
 import Navbar from '../navbar/Navbar'
 
-
 const Header = () => {
   return (
     <>
-    <div className={styles.header}>
-        <div className={styles.header_logo}>
-            <img src={logo} alt="logo" />
-        </div>
-        <div className={styles.header_right_part}>
-            <div>
-                <Navbar />
+    <div className='container'>
+        <div className={styles.header}>
+            <div className={styles.header_logo}>
+                <img src={logo} alt="logo" />
             </div>
-            <div className={styles.header_contact}>
-                <a href="tel:+79000009000">+7 (900) 000-90-00</a>
+            <div className={styles.header_right_part}>
+                <div>
+                    <Navbar />
+                </div>
+                <div className={styles.header_contact}>
+                    <a href="tel:+79000009000">+7 (900) 000-90-00</a>
+                </div>
             </div>
         </div>
     </div>
