6ddd2b6f9c2d3da225ce76cc0ab34d7836cbf19c
Konstantin <kostya1703tsoy@gmail.com>
2024-02-08 22:27:06 +0300

    fix moreDetailsPage

--- frontend/src/components/moreDetails/MoreDetails.jsx
+++ frontend/src/components/moreDetails/MoreDetails.jsx
@@ -1,27 +1,21 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import styles from "./MoreDetails.module.css"
 
 import classNames from 'classnames'
 
-import { Link } from 'react-router-dom'
-
 import Map from "../../img/Map.png"
 
 const MoreDetails = () => {
-  useEffect(() => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  }, []);
 
   const [showAlert, setShowAlert] = useState(false);
 
   const handleClick = () => {
     setShowAlert(true);
   };
-
+  
   return (
     <>
     <div className={ classNames(styles.moreDetails, "container")}>
-      <Link to="/"><button className='button'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-left"><path d="m12 19-7-7 7-7"/><path d="M19 12H5"/></svg></button></Link>
       <div className={styles.moreDetails_info}>
         <div className={styles.moreDetails_info_left}>
 
